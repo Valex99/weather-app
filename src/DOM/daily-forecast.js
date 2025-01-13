5bd4856188625a78aa418dd5a4ade04dd9e5e63a
valex <val.matej99@gmail.com>
2025-01-13 01:19:42 +0100

    Make daily forecast work

--- src/DOM/daily-forecast.js
+++ src/DOM/daily-forecast.js
@@ -1,6 +1,7 @@
 // 3
 import calendarIcon from "../project-icons/calendar-icon.png";
 import { windMap } from "./wind-map";
+import clearIcon from "../weather-icons/clear.png";
 
 function dailyForecast() {
   // 1. Main div
@@ -29,11 +30,47 @@ function dailyForecast() {
 
   dailyForecastDiv.appendChild(iconTitleDiv);
 
+  // Call eachHour for each value
+  for (let i = 0; i < 10; i++) {
+    eachDay(i, dayByDayDiv);
+  }
+
+
   dailyForecastDiv.appendChild(dayByDayDiv);
 
   content.appendChild(dailyForecastDiv);
 
   windMap();
+}
+
+// Create daily forecast
+function eachDay(n, elementToAppendTo) {
+  const dayDiv = document.createElement("div");
+  dayDiv.classList.add("day-div");
+
+  const day = document.createElement("p");
+  day.textContent = "Mon";
+
+  const weatherIcon = document.createElement("img");
+  weatherIcon.src = clearIcon;
+  weatherIcon.classList.add("weather-icon")
+
+  const minTemp = document.createElement("p");
+  minTemp.textContent = "-2°";
+
+  const graph = document.createElement("p");
+  graph.textContent = "--------";
+
+  const maxTemp = document.createElement("p");
+  maxTemp.textContent = "4°";
+
+  dayDiv.appendChild(day);
+  dayDiv.appendChild(weatherIcon);
+  dayDiv.appendChild(minTemp);
+  dayDiv.appendChild(graph);
+  dayDiv.appendChild(maxTemp);
+
+  elementToAppendTo.appendChild(dayDiv);
 }
 
 export { dailyForecast };
